f759e69187918628a1a0943decc3984dcf92a54b
Vikas Sharma <93978146+vikasharma005@users.noreply.github.com>
2023-08-21 19:21:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ conn = GitHubConnection("github")
 st.set_page_config(layout='wide')
 
 # Sidebar
-st.sidebar.image("github-logo.png", width=100)
+st.sidebar.image("https://github.com/vikasharma005/GitRep/blob/main/github-logo.png", width=100)
 st.sidebar.title("GitHub User Explorer 🚀")
 github_username = st.sidebar.text_input("Enter GitHub Username:")
 
